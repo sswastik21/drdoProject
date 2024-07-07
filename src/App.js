b53561fc76b2333e9d2d815507fbe541a15b6f69
3shibani <98184469+3shibani@users.noreply.github.com>
2024-07-07 15:17:47 +0530

    HomePage added

--- src/App.js
+++ src/App.js
@@ -3,29 +3,28 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Pages/Navbar'
 import Dashboard from './Pages/Dashboard';
-  import SelectionPage from './Pages/SelectionPage';
-  import AdminLoginPage from './Pages/AdminLoginPage';
-  import SignupPage from './Pages/SignupPage';
+import SelectionPage from './Pages/SelectionPage';
+import AdminLoginPage from './Pages/AdminLoginPage';
+import SignupPage from './Pages/SignupPage';
+import HomePage from './Pages/HomePage';
+
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Dashboard/>
-      {/* <SelectionPage/> */}
-    </div>
-     // UNCOMMENT THE BELOW PART AND COMMENT OUT THE ABOVE PART TO VIEW THE login signup part
-  //   <Router>
-  //   <Switch>
-  //     <Route exact path="/" component={SelectionPage} />
-  //     <Route exact path="/admin-login" component={AdminLoginPage} />
-  //     <Route exact path="/user-login" component={AdminLoginPage} />
-  //     <Route exact path="/signup" component={SignupPage} />
-      
-      
-
-  //   </Switch>
-  // </Router>
-
+    // <div className="App">
+    //   <Navbar/>
+    //   <Dashboard/>
+    //   <SelectionPage/>
+    // </div>
+    <Router>
+      <Switch>
+      {/* UNCOMMENT THE BELOW PART AND COMMENT OUT THE ABOVE PART TO VIEW THE login signup part */}
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/selection" component={SelectionPage} />
+        <Route exact path="/admin-login" component={AdminLoginPage} />
+        <Route exact path="/user-login" component={AdminLoginPage} /> 
+        <Route exact path="/signup" component={SignupPage} />
+      </Switch>
+    </Router>
   );
 }
 
