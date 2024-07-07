aed6ecf10ba8e3a8fa6017cb6d88626828c8bbae
unknown <adyamishra02@gmail.com>
2024-07-07 16:48:34 +0530

    temporary navbar linking

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './Pages/Navbar'
 import Dashboard from './Pages/Dashboard';
-import About from './Pages/About'
+import About from './Pages/About';
+import Home from './Pages/HomePage'
   import SelectionPage from './Pages/SelectionPage';
    import AdminLoginPage from './Pages/AdminLoginPage';
   import SignupPage from './Pages/SignupPage';
@@ -16,11 +17,13 @@ function App() {
     <Router>
      <div>
     <Switch>
-      <Route exact path="/" component={SelectionPage} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/selection" component={SelectionPage} />
       <Route exact path="/admin-login" component={AdminLoginPage} />
       <Route exact path="/user-login" component={AdminLoginPage} />
       <Route exact path="/dashboard" component={Dashboard} />
       <Route exact path="/signup" component={SignupPage} />    
+      <Route exact path="/about" component={About} />    
     </Switch>
     </div>
   </Router>
